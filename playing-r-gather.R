a4b199985e7d3cb82064859ab21083f8673a7cba
Brock Tibert <btibert3@gmail.com>
2019-03-13 14:46:08 -0400

    update with docker comppose

--- playing-r-gather.R
+++ playing-r-gather.R
@@ -12,14 +12,25 @@ library(ipeds)
 library(tidyverse)
 library(jsonlite)
 
+
+##### Docker Assumptions
+
+## download the .jar files and put in the projects neo4j/plugins dir 
+### will be mapped during the container build locally
+
+## docker containser spinup -- better/easier to manage in terminal within RStudio
+## docker-compose up
+## or, docker-compose up -d
+## docker-compose stop
+
+
+
 # connect to the database
 graph = neo4j_api$new(url = "http://localhost:7474",
                       user = "neo4j",
                       password = "password")
 graph$ping()
 
-## filepath for neo4j
-NEO_PATH = "/Users/btibert/Library/Application Support/Neo4j Desktop/Application/neo4jDatabases/database-498f7f4b-eca6-432f-85da-6f4b68545626/installation-3.5.0/"
 
 ## instantiate the surveys
 data("surveys")
@@ -30,8 +41,23 @@ raw_survey$survey_year = "201617"
 
 ## write a json file
 raw_survey_list = toJSON(raw_survey, dataframe = "rows")
-write_json(raw_survey_list, paste0(NEO_PATH,"/import/","survey.json"))
+write_json(raw_survey_list, "neo4j/import/survey.json")
+
+## write a test file
+test_survey = toJSON(raw_survey[1,], dataframe = "rows")
+write_json(test_survey, "neo4j/import/test-survey.json")
 
 
 ## apoc cql to import the file -- written in browser
+CQL = "
+WITH 'file:///test-survey.json' as url
+CALL apoc.load.json(url) YIELD value
+WITH value
+LIMIT 1
+RETURN value
+"
+tmp = call_neo4j(CQL, graph, type="graph")
+
+
+
 
